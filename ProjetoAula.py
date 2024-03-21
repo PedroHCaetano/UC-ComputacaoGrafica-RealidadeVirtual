a649ac7c3f28f715c5128716a5008376f7ebd704
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-03-21 10:11:00 -0300

    Criando tela com GLFW e OpenGL

---
+++ ProjetoAula.py
@@ -0,0 +1,36 @@
+import glfw
+from OpenGL.GL import *
+
+if not glfw.init():
+    raise Exception("GLFW não pode ser executado")
+window = glfw.create_window(1000, 600, "Uma janela para o seu bruxo", None, None)
+
+glfw.set_window_pos(window, 400, 100)
+glfw.make_context_current(window)
+
+def center_window(window):
+    # Obter dimensões da tela
+    monitor = glfw.get_primary_monitor()
+    screen_dimension = glfw.get_video_mode(monitor)
+    screen_width, screen_heigth = screen_dimension.size
+
+    # Obter dimensões da janela
+    window_width, window_heigth = glfw.get_window_size(window)
+
+    # Calcular as coordenadas x e y do canto superior esquerdo da janela
+    x_pos = (screen_width - window_width) // 2
+    y_pos = (screen_heigth - window_heigth) // 2
+
+    # Configurar a posição da janela
+    glfw.set_window_pos(window, x_pos, y_pos)
+
+center_window(window)
+
+glClearColor(0.4, 0.2, 0.1, 1.0)
+
+while not glfw.window_should_close(window):
+    glClear(GL_COLOR_BUFFER_BIT)
+
+    glfw.poll_events()
+    glfw.swap_buffers(window)
+glfw.terminate()
---
+++ README.md
@@ -0,0 +1,9 @@
+<h1>Computacao Grafica e Realidade Virtual</h1>
+
+<h2>🚀 Sobre</h2>
+<p>Repositório feito para guardar os projetos e atividades da UC de Computação Gráfica e Realidade Virtual.</p>
+
+## 📋 Tecnologias
+<div>
+  <img src="https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54">
+</div>
