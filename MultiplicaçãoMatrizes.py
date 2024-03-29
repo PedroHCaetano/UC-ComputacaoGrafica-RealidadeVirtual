911b5e67a5ccf8ec164e51cfe02f7a29816e05c5
PedroHCaetano <pedro.henricaetano1405@outlook.com>
2024-03-29 10:44:16 -0300

    Novo Arquivo da Aula

---
+++ MultiplicaçãoMatrizes.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+# Matriz A
+A = np.array( [[1, 3, 5],
+               [6, 32, 1],
+               [5, 3, 9] ] )
+print(f"A = {A}")
+
+print(" ") # Deixei vazio para separar as linhas ao in vés de usar o \n
+
+# Matriz B
+B = np.array( [[3, 7, 1],
+               [5, 2, 7],
+               [9, 8, 9]])
+print(f"B = {B}")
